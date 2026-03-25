08dfd5a9f9a4bf8030edb6a2bcdaf06813106d3a
Envo1d <79973093+Envo1d@users.noreply.github.com>
2026-03-25 11:37:07 +0200

    feat: project name and icon

---
+++ build.rs
@@ -0,0 +1,13 @@
+use std::env;
+
+fn main() {
+    if env::var("CARGO_CFG_WINDOWS").is_ok() {
+        let mut res = winres::WindowsResource::new();
+
+        res.set_icon("assets/icon.ico");
+
+        if let Err(e) = res.compile() {
+            eprintln!("Ошибка при компиляции ресурсов Windows: {}", e);
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use crate::ui::app::MediaApp;
+use std::sync::Arc;
 
 mod core;
 mod data;
@@ -7,15 +8,19 @@ mod ui;
 mod utils;
 
 fn main() -> eframe::Result {
+    let icon_data = eframe::icon_data::from_png_bytes(include_bytes!("../assets/icon.png"))
+        .expect("Не удалось загрузить иконку окна");
+
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_inner_size([1200.0, 800.0])
-            .with_title("Oxide View"),
+            .with_title("Nexa")
+            .with_icon(Arc::new(icon_data)),
         ..Default::default()
     };
 
     eframe::run_native(
-        "oxide_view_app",
+        "nexa_app",
         native_options,
         Box::new(|cc| Ok(Box::new(MediaApp::new(cc)))),
     )
